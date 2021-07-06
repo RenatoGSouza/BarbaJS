dce965b95a76f8fbf9394220c4d7889cd2fe13a1
Renato GSouza <renatomacedo1995@gmail.com>
2021-07-06 17:08:39 -0300

    fim do tutorial

--- index.js
+++ index.js
@@ -1,58 +1,15 @@
 
-barba.hooks.leave(data => {
-  console.log('This is a global hook!');
-});
-
-function transitionFuc(helo, obj) {
-  console.log(helo);
-  obj.subFunction();
-}
-
 barba.use(barbaCss);
 
 barba.init({
-  requestError: (trigger, action, url, response) => {
-    if(response.status == 404)
-      barba.go('./NotFound.html');
-    return false;
-  },
-  views: [{
-    namespace: 'dino1',
-    beforeLeave() {
-      console.log('beforeLeave dino1');
-    }
-  },
-  {
-    namespace: 'dino3',
+  transitions: [{
+    enter() {
+      //This code will not be executed
+      console.log("Hello, I'm the enter hook!");
+    },
     afterEnter() {
-      console.log('afterEnter dino3');
+     //This code will be executed
+     console.log("Hello, I'm the beforeEnter hook!");
     }
-  }
-],
-  transitions:[
-    {
-      name: 'Amazing transition',
-      from: {
-        namespace:['dino3']
-      },
-      enter(data) {
-        console.log('amzing Transition has benn applied!');
-      }
-    },
-    {
-      name: 'Gorgeous transition',
-      from: {
-        namespace: ['dino1']
-       },
-       to: {
-         namespace: ['dino2']
-       }, 
-       enter(data) {
-        console.log('Gorgeous transition has been applied!');
-       }
-    },
-    {
-      name: 'Beautiful transition'
-    },
-  ]
-})
+}]
+});
